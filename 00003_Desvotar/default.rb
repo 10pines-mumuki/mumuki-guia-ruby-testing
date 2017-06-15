7f8ef29676aeed1711444b5ad4409e1ba31a015a
umanskyariel@gmail.com <umanskyariel@gmail.com>
2017-06-15 15:55:36 -0400

    Mumuki Export on 2017-06-15 15:55:36 -0400

---
+++ 00003_Desvotar/default.rb
@@ -0,0 +1,49 @@
+class ReunionDemocratica
+  def initialize
+    @temas = []
+  end
+  def votar!(tema, usuario)
+    if(usuario.cantidad_votos_disponibles > 0)
+      tema.sumar_voto!
+      usuario.restar_voto_disponible!
+    else 
+      raise StandardError.new('El usuario no tiene mas votos')
+    end
+  end
+  
+  def desvotar!(tema, usuario)
+    tema.restar_voto!(usuario)
+    usuario.sumar_voto_disponible!
+  end
+end
+
+class Usuario
+  def initialize
+    @votos_disponibles = 3
+  end
+  def restar_voto_disponible!
+    @votos_disponibles -= 1
+  end
+  def sumar_voto_disponible!
+    @votos_disponibles += 1
+  end
+  def cantidad_votos_disponibles
+    @votos_disponibles
+  end
+end
+
+class Tema
+  def initialize
+    @votos = 0
+  end
+  def sumar_voto!
+    @votos += 1
+  end
+  def restar_voto!
+    @votos -= 1
+  end
+end
+
+una_reunion_democratica = ReunionDemocratica.new
+un_usuario = Usuario.new
+un_tema = Tema.new
---
+++ 00003_Desvotar/description.md
@@ -0,0 +1,8 @@
+Otro desarrollador acaba de agregar una feature mientras nosotros corregíamos el bug en `votar!`.
+
+Básicamente, esa feature consiste en sacar el voto de que un usuario le haya dado a un tema.
+
+Aunque el desarrollador dijo que probó esta funcionalidad, recibimos reportes de que el sistema no se comporta correctamente.
+
+> ¿Podés detectar dónde está el problema?
+
