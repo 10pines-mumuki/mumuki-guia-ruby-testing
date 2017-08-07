2e166f324e667a337459a99345f897b039453a0e
franco@mumuki.org <franco@mumuki.org>
2017-08-07 14:19:19 -0400

    Mumuki Export on 2017-08-07 14:19:19 -0400

--- 00006_Minitest - Tests/default.rb
+++ 00006_Minitest - Tests/default.rb
@@ -2,3 +2,4 @@ class ReunionEstrategicaTest < Minitest::Test
   
   
 end
+
--- 00006_Minitest - Tests/test.rb
+++ 00006_Minitest - Tests/test.rb
@@ -1,21 +1,90 @@
-#TODO
-
-=begin
-
-class ReunionEstrategicaTest < Minitest::Test
-  
-  def test_votar_en_reunion_suma_un_voto_al_tema_pasado
-    
-    usuario = Usuario.new
-    tema = Tema.new
-    reunion = ReunionEstrategica.new
-    
-    reunion.votar!(tema, usuario)
-    
-    assert_equals 1, tema.votos
-    
-  end
-  
-end
-
-=end
+examples:
+  - name: 'si el código es correcto, los tests pasan'
+    fixture: |
+      class Asamblea
+      
+      end
+      
+      class Asambleista
+        def initialize
+          @votos_disponibles = 3
+        end
+        def restar_voto_disponible!
+          @votos_disponibles -= 1
+        end
+        def sumar_voto_disponible!
+          @votos_disponibles += 1
+        end
+        def cantidad_votos_disponibles
+          @votos_disponibles
+        end
+      end
+      
+      class Tema
+        def initialize
+          @votantes = []
+        end
+        def sumar_voto!(usuario)
+          @votantes << usuario
+        end
+        def restar_voto!(usuario)
+          if(@votantes.contains?(usuario))
+            @votantes.delete_at(@votantes.find_index(usuario))
+          end
+        end
+        def votos
+          @votos
+        end
+      end    
+    postconditions:
+      status: passed
+      
+  - name: 'si sumar voto no está implementado, deberían fallar los tests'
+    fixture: |
+      class Asamblea
+        def votar!(tema, usuario)
+          if(usuario.cantidad_votos_disponibles > 0)
+            tema.sumar_voto!
+            usuario.restar_voto_disponible!
+          else 
+            raise StandardError.new('El usuario no tiene mas votos')
+          end
+        end
+        def desvotar!(tema, usuario)
+          tema.restar_voto!(usuario)
+          usuario.sumar_voto_disponible!
+        end
+      end
+      
+      class Asambleista
+        def initialize
+          @votos_disponibles = 3
+        end
+        def restar_voto_disponible!
+          @votos_disponibles -= 1
+        end
+        def sumar_voto_disponible!
+          @votos_disponibles += 1
+        end
+        def cantidad_votos_disponibles
+          @votos_disponibles
+        end
+      end
+      
+      class Tema
+        def initialize
+          @votantes = []
+        end
+        def sumar_voto!(usuario)
+        end
+        def restar_voto!(usuario)
+          if(@votantes.contains?(usuario))
+            @votantes.delete_at(@votantes.find_index(usuario))
+          end
+        end
+        def votos
+          @votos
+        end
+      end    
+    postconditions:
+      status: failed
