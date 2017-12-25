f3ae9f63b5ec7b24e60856417b299ed4417304b2
umanskyariel@gmail.com <umanskyariel@gmail.com>
2017-12-25 18:03:33 -0500

    Mumuki Export on 2017-12-25 18:03:33 -0500

--- 00002_Bugfixin' a manopla/test.rb
+++ 00002_Bugfixin' a manopla/test.rb
@@ -1,24 +1,35 @@
 describe "Asamblea" do 
+  subject do
+    una_asamblea.votar! un_tema, un_asambleista
+  end
+  
   describe "si un asambleista vota una vez" do 
-    before { una_asamblea.votar! un_tema, un_asambleista  }
-    it "el asambleista pierde un voto" do 
-      expect(una_asamblea.votos).to eq 1
+
+    it "el asambleista pierde un voto" do
+      expect(un_asambleista.votos).to eq 3
+      subject
+      expect(un_asambleista.votos).to eq 2
     end
+    
     it "el tema gana un voto" do
+      expect(un_tema.votos).to eq 0
+      subject
       expect(un_tema.votos).to eq 1
     end
   end
   
   describe "si el asambleista vota tres veces" do 
-    before { 2.times { una_asamblea.votar! un_tema, un_asambleista }  }
+    before do
+      3.times { subject }
+    end
 
     it "la votación falla y el asambleista no pierde votos" do 
-      expect { una_asamblea.votar! un_tema, un_asambleista  }.to raise_error
+      expect { subject  }.to raise_error
       expect(una_asambleista.votos_disponibles).to eq 0
     end
     
     it "el tema no gana votos" do 
-      una_asamblea.votar! un_tema, un_asambleista rescue nil
+      expect { subject  }.to raise_error
       expect(un_tema.votos).to eq 3
     end
   end
