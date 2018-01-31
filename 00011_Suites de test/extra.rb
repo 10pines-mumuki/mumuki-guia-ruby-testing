da0ac30611fd0ba129c48bc02bc0aaa1f0bf5021
umanskyariel@gmail.com <umanskyariel@gmail.com>
2018-01-31 18:43:56 -0500

    Mumuki Export on 2018-01-31 18:43:56 -0500

---
+++ 00011_Suites de test/extra.rb
@@ -0,0 +1,56 @@
+class AsambleaTest < Minitest::Test
+  def test_votar_en_asamblea_suma_un_voto_al_tema
+    asambleista = Asambleista.new
+    tema = Tema.new
+    asamblea = Asamblea.new
+    
+    asamblea.votar!(tema, asambleista)
+    
+    assert_equal 1, tema.votos
+  end
+  
+  def test_votar_en_asamblea_resta_un_voto_al_asambleista
+    asambleista = Asambleista.new
+    tema = Tema.new
+    asamblea = Asamblea.new
+    
+    asamblea.votar!(tema, asambleista)
+    
+    assert_equal 2, asambleista.votos
+  end
+end
+
+class Asambleista
+  def initialize
+    @votos_disponibles = 3
+  end
+  def restar_voto_disponible!
+    @votos_disponibles -= 1
+  end
+  def sumar_voto_disponible!
+    @votos_disponibles += 1
+  end
+  def votos
+    @votos_disponibles
+  end
+end
+
+class Tema
+  def initialize
+    @votantes = []
+    @votos = 0
+  end
+  def sumar_voto!(asambleista)
+    asambleista.restar_voto_disponible!
+    @votantes << asambleista
+    @votos = @votos + 1
+  end
+  def restar_voto!(asambleista)
+    if(@votantes.include?(asambleista))
+      @votantes.delete_at(@votantes.find_index(asambleista))
+    end
+  end
+  def votos
+    @votos
+  end
+end    
