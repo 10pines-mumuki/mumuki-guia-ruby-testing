ee63437de8556faa6066e0323b38e0b01e3fd8f0
umanskyariel@gmail.com <umanskyariel@gmail.com>
2018-01-31 18:37:56 -0500

    Mumuki Export on 2018-01-31 18:37:56 -0500

--- 00010_Mas tests/test.rb
+++ 00010_Mas tests/test.rb
@@ -1,21 +1,55 @@
-#TODO
+examples:
+- name: 'si el codigo es correcto, los tests pasan'
+  fixture: |
+    class Asamblea
+      def votar!(tema, asambleista)
+        raise 'El asambleista no tiene mas votos'if asambleista.votos <= 0
+        
+        tema.sumar_voto! asambleista
+        asambleista.restar_voto_disponible!
+      end
+      
+      def desvotar!(tema, asambleista)
+        tema.restar_voto!(asambleista)
+        asambleista.sumar_voto_disponible!
+      end
+    end
+  postconditions:
+      status: passed
 
-=begin
+- name: 'si el assert está mal, debería fallar el ejercicio'
+  fixture: |
+    class Asamblea
+      def votar!(tema, asambleista)
+        raise 'El asambleista no tiene mas votos'if asambleista.votos <= 0
+        
+        tema.sumar_voto! asambleista
+        tema.sumar_voto! asambleista
+        tema.sumar_voto! asambleista
+        asambleista.restar_voto_disponible!
+      end
+      
+      def desvotar!(tema, asambleista)
+        tema.restar_voto!(asambleista)
+        asambleista.sumar_voto_disponible!
+      end
+    end
+  postconditions:
+      status: failed
 
-class ReunionEstrategicaTest < Minitest::Test
-  
-  def test_votar_en_reunion_resta_un_voto_al_usuario_pasado
-    
-    usuario = Usuario.new
-    tema = Tema.new
-    reunion = ReunionEstrategica.new
-    
-    reunion.votar!(tema, usuario)
-    
-    assert_equals 2, usuario.cantidad_votos_disponibles
-    
-  end
-  
-end
-
-=end
+- name: 'si el bug persiste, deberían fallar los tests'
+  fixture: |
+    class Asamblea
+      def votar!(tema, asambleista)
+        raise 'El asambleista no tiene mas votos'if asambleista.votos <= 0
+        tema.sumar_voto!
+        asambleista.restar_voto_disponible!
+      end
+      
+      def desvotar!(tema, asambleista)
+        tema.restar_voto!(asambleista)
+        asambleista.sumar_voto_disponible!
+      end
+    end
+  postconditions:
+      status: failed
