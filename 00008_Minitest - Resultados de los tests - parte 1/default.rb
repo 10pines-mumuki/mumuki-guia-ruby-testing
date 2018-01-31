ebb7078e747a7a4d0357ca9f7918be790cb2996f
umanskyariel@gmail.com <umanskyariel@gmail.com>
2018-01-31 18:21:33 -0500

    Mumuki Export on 2018-01-31 18:21:33 -0500

--- 00008_Minitest - Resultados de los tests - parte 1/default.rb
+++ 00008_Minitest - Resultados de los tests - parte 1/default.rb
@@ -1,19 +1,30 @@
-class ReunionDemocratica
-  def votar!(tema, usuario)
-    if(usuario.cantidad_votos_disponibles > 0)
-      tema.sumar_voto!
-      usuario.restar_voto_disponible!
-    else 
-      raise StandardError.new('El usuario no tiene mas votos')
-    end
+class AsambleaTest < Minitest::Test
+  def test_votar_en_asamblea_suma_un_voto_al_tema
+    asambleista = Asambleista.new
+    tema = Tema.new
+    asamblea = Asamblea.new
+    
+    asamblea.votar!(tema, asambleista)
+    
+    assert_equal 1, tema.votos
+  end   
+end
+
+class Asamblea
+  def votar!(tema, asambleista)
+    raise 'El asambleista no tiene mas votos'if asambleista.votos <= 0
+    
+    tema.sumar_voto!
+    asambleista.restar_voto_disponible!
   end
-  def desvotar!(tema, usuario)
-    tema.restar_voto!(usuario)
-    usuario.sumar_voto_disponible!
+  
+  def desvotar!(tema, asambleista)
+    tema.restar_voto!(asambleista)
+    asambleista.sumar_voto_disponible!
   end
 end
 
-class Usuario
+class Asambleista
   def initialize
     @votos_disponibles = 3
   end
@@ -23,7 +34,7 @@ class Usuario
   def sumar_voto_disponible!
     @votos_disponibles += 1
   end
-  def cantidad_votos_disponibles
+  def votos
     @votos_disponibles
   end
 end
@@ -31,20 +42,20 @@ end
 class Tema
   def initialize
     @votantes = []
+    @votos = 0
   end
-  def sumar_voto!(usuario)
-    @votantes << usuario
+  def sumar_voto!(asambleista)
+    asambleista.restar_voto_disponible!
+    @votantes << asambleista
+    @votos = @votos + 1
   end
-  def restar_voto!(usuario)
-    if(@votantes.contains?(usuario))
-      @votantes.delete_at(@votantes.find_index(usuario))
+  def restar_voto!(asambleista)
+    if(@votantes.include?(asambleista))
+      @votantes.delete_at(@votantes.find_index(asambleista))
     end
   end
   def votos
     @votos
   end
-end
+end    
 
-una_reunion_democratica = ReunionDemocratica.new
-un_usuario = Usuario.new
-un_tema = Tema.new
