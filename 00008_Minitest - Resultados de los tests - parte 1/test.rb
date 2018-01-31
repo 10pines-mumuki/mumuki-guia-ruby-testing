c6d75b504fae3ac72b8f6bd66f7113b1d2e82067
umanskyariel@gmail.com <umanskyariel@gmail.com>
2018-01-31 18:23:27 -0500

    Mumuki Export on 2018-01-31 18:23:27 -0500

--- 00008_Minitest - Resultados de los tests - parte 1/test.rb
+++ 00008_Minitest - Resultados de los tests - parte 1/test.rb
@@ -1,1 +1,40 @@
-#TODO
+describe "Asamblea" do 
+  let(:un_tema) { Tema.new }
+  let(:un_asambleista) { Asambleista.new }
+  let(:una_asamblea) { Asamblea.new }
+  
+  subject do
+    una_asamblea.votar! un_tema, un_asambleista
+  end
+  
+  describe "si un asambleista vota una vez" do 
+
+    it "el asambleista pierde un voto" do
+      expect(un_asambleista.votos).to eq 3
+      subject
+      expect(un_asambleista.votos).to eq 2
+    end
+    
+    it "el tema gana un voto" do
+      expect(un_tema.votos).to eq 0
+      subject
+      expect(un_tema.votos).to eq 1
+    end
+  end
+  
+  describe "si el asambleista vota tres veces" do 
+    before do
+      3.times { una_asamblea.votar! un_tema, un_asambleista }
+    end
+
+    it "la votación falla por lo que el asambleista no pierde votos" do 
+      expect { una_asamblea.votar! un_tema, un_asambleista  }.to raise_error
+      expect(un_asambleista.votos).to eq 0
+    end
+    
+    it "la votación falla por lo que el tema no gana votos" do 
+      expect { una_asamblea.votar! un_tema, un_asambleista }.to raise_error
+      expect(un_tema.votos).to eq 3
+    end
+  end
+end
