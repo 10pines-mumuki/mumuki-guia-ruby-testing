45dd9ae645feed37c1fb41b00d2d5762e40f2505
umanskyariel@gmail.com <umanskyariel@gmail.com>
2018-01-31 18:24:13 -0500

    Mumuki Export on 2018-01-31 18:24:13 -0500

--- 00008_Minitest - Resultados de los tests - parte 1/default.rb
+++ 00008_Minitest - Resultados de los tests - parte 1/default.rb
@@ -1,16 +1,16 @@
-    class Asamblea
-      def votar!(tema, asambleista)
-        raise 'El asambleista no tiene mas votos'if asambleista.votos <= 0
-        
-        tema.sumar_voto!
-        asambleista.restar_voto_disponible!
-      end
-      
-      def desvotar!(tema, asambleista)
-        tema.restar_voto!(asambleista)
-        asambleista.sumar_voto_disponible!
-      end
-    end
+class Asamblea
+  def votar!(tema, asambleista)
+    raise 'El asambleista no tiene mas votos'if asambleista.votos <= 0
+    
+    tema.sumar_voto!
+    asambleista.restar_voto_disponible!
+  end
+  
+  def desvotar!(tema, asambleista)
+    tema.restar_voto!(asambleista)
+    asambleista.sumar_voto_disponible!
+  end
+end
 
 class Asambleista
   def initialize
