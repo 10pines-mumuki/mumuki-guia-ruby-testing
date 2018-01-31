378e00b7bc81383072397a8c22846c5143006e43
umanskyariel@gmail.com <umanskyariel@gmail.com>
2018-01-31 18:38:58 -0500

    Mumuki Export on 2018-01-31 18:38:58 -0500

--- 00010_Mas tests/test.rb
+++ 00010_Mas tests/test.rb
@@ -35,26 +35,6 @@ examples:
   postconditions:
       status: failed
 
-- name: 'si el assert está mal, debería fallar el ejercicio'
-  fixture: |
-    class Asamblea
-      def votar!(tema, asambleista)
-        raise 'El asambleista no tiene mas votos'if asambleista.votos <= 0
-        
-        tema.sumar_voto! asambleista
-        tema.sumar_voto! asambleista
-        tema.sumar_voto! asambleista
-        asambleista.restar_voto_disponible!
-      end
-      
-      def desvotar!(tema, asambleista)
-        tema.restar_voto!(asambleista)
-        asambleista.sumar_voto_disponible!
-      end
-    end
-  postconditions:
-      status: failed
-
 - name: 'si el bug persiste, deberían fallar los tests'
   fixture: |
     class Asamblea
