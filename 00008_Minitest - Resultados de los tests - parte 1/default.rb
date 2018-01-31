5211d023cf957b9048af09528b80822196894055
umanskyariel@gmail.com <umanskyariel@gmail.com>
2018-01-31 18:28:06 -0500

    Mumuki Export on 2018-01-31 18:28:06 -0500

--- 00008_Minitest - Resultados de los tests - parte 1/default.rb
+++ 00008_Minitest - Resultados de los tests - parte 1/default.rb
@@ -16,12 +16,15 @@ class Asambleista
   def initialize
     @votos_disponibles = 3
   end
+  
   def restar_voto_disponible!
     @votos_disponibles -= 1
   end
+  
   def sumar_voto_disponible!
     @votos_disponibles += 1
   end
+  
   def votos
     @votos_disponibles
   end
@@ -32,16 +35,19 @@ class Tema
     @votantes = []
     @votos = 0
   end
+  
   def sumar_voto!(asambleista)
     asambleista.restar_voto_disponible!
     @votantes << asambleista
     @votos = @votos + 1
   end
+  
   def restar_voto!(asambleista)
     if(@votantes.include?(asambleista))
       @votantes.delete_at(@votantes.find_index(asambleista))
     end
   end
+  
   def votos
     @votos
   end
