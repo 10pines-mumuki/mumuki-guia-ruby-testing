b048793f92426de34989e4de1cee5269700d7f19
franco@mumuki.org <franco@mumuki.org>
2018-02-07 22:44:23 -0500

    Mumuki Export on 2018-02-07 22:44:23 -0500

--- 00012_Últimos tests/test.rb
+++ 00012_Últimos tests/test.rb
@@ -62,7 +62,9 @@ examples:
       def votar!(tema, asambleista)
         raise 'El asambleista no tiene mas votos'if asambleista.votos <= 0
         
+        
         tema.sumar_voto! asambleista
+        asambleista.restar_voto_disponible!
       end
       
       def desvotar!(tema, asambleista)
